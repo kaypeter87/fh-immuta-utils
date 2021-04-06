7600c89f6403d1a612d28fecc4f0f2dc48911e31
Peter Kay <peter.kay@flatiron.com>
2021-04-06 15:26:09 -0400

    add test metrics client

---
+++ fh_immuta_utils/metrics.py
@@ -0,0 +1,14 @@
+from datadog import initialize, statsd
+import time
+
+options = {
+    "statsd_host": "127.0.0.1",
+    "statsd_port": 8125,
+}
+
+initialize(**options)
+
+while(1):
+    statsd.increment('test.increment', tags=["environment:dev"])
+    statsd.decrement('test.decrement', tags=["environment:dev"])
+    time.sleep(10)

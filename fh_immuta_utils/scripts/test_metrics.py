bd898afd8ef0d7b8635be18b85f6190ec996679c
Peter Kay <peter.kay@flatiron.com>
2021-04-27 15:31:08 -0400

    update version in setup and add click command to test_metrics

--- fh_immuta_utils/scripts/test_metrics.py
+++ fh_immuta_utils/scripts/test_metrics.py
@@ -1,13 +1,16 @@
 from fh_immuta_utils.metrics import test_metrics
+import click
 
 
+@click.command(help="Sending test metrics to datadog")
 def cli_entrypoint():
-    main()
+    return main()
 
 
 def main():
     print("sending test metrics to datadog...")
     test_metrics()
+    return 0
 
 
 if __name__ == "__main__":
